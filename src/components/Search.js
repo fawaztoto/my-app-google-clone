f715898911ebb9f4fcc0ad41ed4529861a9f2d6f
fawaztoto <fawaztotonchy2@gmail.com>
2022-11-14 17:23:53 +0800

    Google clone, pending

---
+++ src/components/Search.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import './Search.css'
+import SearchIcon from '@mui/icons-material/Search';
+import MicIcon from '@mui/icons-material/Mic';
+import  Button from '@mui/material/Button'
+import { useHistory } from 'react-router-dom';
+
+
+
+
+
+function Search() {
+ const [input, setInput] = useState('')
+const history = useHistory()
+
+
+const search = e => {
+    e.preventDefault();
+}
+
+  return (
+    <form className='search'>
+        <div className='search__input'>
+            <SearchIcon className="search__inputIcon" /> 
+            <input value={input} onChange={e => setInput(e.target.value)} />
+            <MicIcon />
+          </div>
+          
+          <div className='search__buttons'>
+           <Button type='submit' onClick={search} variant="outlined">Google Search</Button>
+           <Button variant="outlined">I'm Feeling Lucky</Button>
+          </div>
+    </form>
+  )
+}
+
+export default Search
---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import Avatar from "@mui/icons-material/AccountBoxOutlined";
+import Search from "../components/Search";
+
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
+
+        <div className="home__headerRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
+
+      <div className="home__body">
+        <img
+          src="https://1000logos.net/wp-content/uploads/2021/05/Google-logo.png"
+          alt=""
+        />
+
+        <div className="home__inputContainer">
+          <Search />
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
